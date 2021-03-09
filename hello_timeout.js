e7bf97cab46bae2aa98dc159b14fd9cc6cff95cd
Gabriel <gabriel.jchen@gmail.com>
2021-03-09 18:22:42 +0000

    first commit

---
+++ hello_timeout.js
@@ -0,0 +1,17 @@
+// const greeting = () => {
+  
+//   console.log("4311o th3r3 w0r1d");
+  
+//   setTimeout(greeting, 3000);
+// }, 
+
+
+setTimeout(() => {
+  console.log('4311o');
+  }, 1000);
+setTimeout(() => {
+  console.log('th3r3');
+}, 2000);
+setTimeout(() => {
+  console.log('w0r1d');
+}, 3000);
